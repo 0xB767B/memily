b02eaea3d69ef18ff9355a786c10dd723f02eec0
0xB767B <marco.schuler@gmail.com>
2015-10-16 17:08:15 +0200

    beautify

--- target/sys/arm/cortex-m3/crt0_init_ram.cpp
+++ target/sys/arm/cortex-m3/crt0_init_ram.cpp
@@ -22,16 +22,16 @@ void crt::init_ram()
   // copy the data segment initializers from rom to ram
   // note: all data segments are aligned by 4
   const std::size_t size = std::size_t(
-          static_cast<const memory_aligned_type *>(static_cast<const void*>(&_data_end))
+          static_cast<const memory_aligned_type*>(static_cast<const void*>(&_data_end))
                                        - static_cast<const memory_aligned_type *>(static_cast<const void*>(&_data_begin)));
 
-  std::copy(static_cast<const memory_aligned_type *>(static_cast<const void*>(&_rom_data_begin)),
-            static_cast<const memory_aligned_type *>(static_cast<const void*>(&_rom_data_begin)) + size,
-            static_cast<      memory_aligned_type *>(static_cast<      void*>(&_data_begin)));
+  std::copy(static_cast<const memory_aligned_type*>(static_cast<const void*>(&_rom_data_begin)),
+            static_cast<const memory_aligned_type*>(static_cast<const void*>(&_rom_data_begin)) + size,
+            static_cast<      memory_aligned_type*>(static_cast<      void*>(&_data_begin)));
 
   // clear  bss segment.
   // note: bss segment is aligned by 4.
-  std::fill(static_cast<memory_aligned_type *>(static_cast<void*>(&_bss_begin)),
-            static_cast<memory_aligned_type *>(static_cast<void*>(&_bss_end)),
+  std::fill(static_cast<memory_aligned_type*>(static_cast<void*>(&_bss_begin)),
+            static_cast<memory_aligned_type*>(static_cast<void*>(&_bss_end)),
             static_cast<memory_aligned_type>(0U));
 }
