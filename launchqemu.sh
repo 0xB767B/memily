12f252cf0285dbe21174b70795d4e9f21b40050a
0xB767B <marco.schuler@gmail.com>
2015-10-19 12:59:39 +0200

    deleted files

--- launchqemu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pkill qemu-system-arm
-qemu-system-arm -cpu cortex-m3 -S -gdb tcp::1234 -nographic -M lm3s6965evb -semihosting -kernel $1
-
--- set_path.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH=/home/borealis/local/toolchains/arm/gcc-arm-none-eabi-4_9-2014q4/bin/:$PATH
-
