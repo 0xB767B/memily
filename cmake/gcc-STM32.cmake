5e0753fa488553b5165041c7c8e057d887a9122d
0xB767B <marco.schuler@gmail.com>
2015-10-23 15:52:49 +0200

    compiler in cmake now must be in path

--- cmake/gcc-STM32.cmake
+++ cmake/gcc-STM32.cmake
@@ -18,28 +18,35 @@
 
 INCLUDE(CMakeForceCompiler)
 
-if(NOT TOOLCHAIN_PREFIX)
-  set(TOOLCHAIN_PREFIX "/home/$ENV{USER}/bin/toolchains/gcc-arm-none-eabi-4_9-2015q3")
-  message(STATUS "No TOOLCHAIN_PREFIX specified, using default: " ${TOOLCHAIN_PREFIX})
-endif()
+#if(NOT TOOLCHAIN_PREFIX)
+#  set(TOOLCHAIN_PREFIX "/home/$ENV{USER}/bin/toolchains/gcc-arm-none-eabi-4_9-2015q3")
+#  message(STATUS "No TOOLCHAIN_PREFIX specified, using default: " ${TOOLCHAIN_PREFIX})
+#endif()
 
 if(NOT TARGET_TRIPLET)
   set(TARGET_TRIPLET "arm-none-eabi")
   message(STATUS "No TARGET_TRIPLET specified, using default: " ${TARGET_TRIPLET})
 endif()
 
-set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
-set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_PREFIX}/${TARGET_TRIPLET}/include)
-set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_PREFIX}/${TARGET_TRIPLET}/lib)
+#set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
+#set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_PREFIX}/${TARGET_TRIPLET}/include)
+#set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_PREFIX}/${TARGET_TRIPLET}/lib)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-CMAKE_FORCE_C_COMPILER(${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-g++ GNU)
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-gcc) # memily should not contain any asm-files
-set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-objcopy CACHE INTERNAL "objcopy tool")
-set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-objdump CACHE INTERNAL "objdump tool")
+#CMAKE_FORCE_C_COMPILER(${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-gcc GNU)
+#CMAKE_FORCE_CXX_COMPILER(${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-g++ GNU)
+#set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-gcc) # memily should not contain any asm-files
+#set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-objcopy CACHE INTERNAL "objcopy tool")
+#set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/${TARGET_TRIPLET}-objdump CACHE INTERNAL "objdump tool")
+
+CMAKE_FORCE_C_COMPILER(${TARGET_TRIPLET}-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(${TARGET_TRIPLET}-g++ GNU)
+set(CMAKE_ASM_COMPILER ${TARGET_TRIPLET}-gcc) # memily should not contain any asm-files
+set(CMAKE_OBJCOPY ${TARGET_TRIPLET}-objcopy CACHE INTERNAL "objcopy tool")
+set(CMAKE_OBJCOPY ${TARGET_TRIPLET}-objdump CACHE INTERNAL "objdump tool")
+
 
 set(CMAKE_C_FLAGS_DEBUG "-mcpu=cortex-m3 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra -g3 -std=gnu11" CACHE INTERNAL "c compiler flags debug") # -MMD -MP -MF"system/src/stm32f1-stdperiph/stm32f10x_gpio.d" -MT"system/src/stm32f1-stdperiph/stm32f10x_gpio.o" -c -o "system/src/stm32f1-stdperiph/stm32f10x_gpio.o"
 set(CMAKE_CXX_FLAGS_DEBUG "-mcpu=cortex-m3 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra -g3 -std=c++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics" CACHE INTERNAL "cxx compiler flags debug") # -MMD -MP -MF"src/BlinkLed.d" -MT"src/BlinkLed.o" -c -o "src/BlinkLed.o"
